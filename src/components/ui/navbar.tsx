eba80da1c59b33124cf01ac5a9c94eb084ad594c
EptesicusDev <eptesicus.dev@gmail.com>
2025-02-16 18:21:57 +0100

    build: navbar

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,91 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Card } from "@/components/ui/card";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { nanoid } from "nanoid";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <Card className="container mx-auto bg-card py-3 px-4 flex items-center justify-between gap-6 shadow-none border-0">
+
+      <ul className="hidden md:flex items-center gap-10 text-card-foreground">
+        <li>
+          <a href="#pricing">Lien</a>
+        </li>
+        <li>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <span className="cursor-pointer">Dropdown</span>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              {landings.map((page) => (
+                <DropdownMenuItem key={page.id}>
+                  <Link href={page.route}>{page.title}</Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </li>
+      </ul>
+
+      <div className="flex items-center">
+        <ModeToggle />
+      </div>
+    </Card>
+  );
+};
+
+const landings = [
+  {
+    id: nanoid(),
+    title: "Landing 01",
+    route: "/project-management",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 02",
+    route: "/crm-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 03",
+    route: "/ai-content-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 04",
+    route: "/new-intro-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 05",
+    route: "/about-us-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 06",
+    route: "/contact-us-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 07",
+    route: "/faqs-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 08",
+    route: "/pricing-landing",
+  },
+  {
+    id: nanoid(),
+    title: "Landing 09",
+    route: "/career-landing",
+  },
+];
+
+export default Navbar;
